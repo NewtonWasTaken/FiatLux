44ef0958a43ab425fb051d32e023868ce499e57a
Fiat Lux <upecenabuchta@gmail.com>
2022-12-15 18:09:34 +0100

    fixed errors after debugging on AstroPi

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from time import sleep
 from picamera import PiCamera
 from pathlib import Path
 from datetime import datetime, timedelta
+import os
 
 #This variable should be on the very top to have the start time accurate
 start_time = datetime.now()
 
 #Folder of where the main.py is stored
 base_folder = Path(__file__).parent.resolve()
+
+#Initializing folder for photos
+os.mkdir(f"{base_folder}/photos")
+
 
 #Initializing main data.csv file
 main_data = open(f"{base_folder}/data.csv", "w")
@@ -20,11 +25,14 @@ camera.resolution = (2592, 1944)
 #This counts how many times the main loop was executed
 counter = 0
 
-#This variable should be as closest to main loop, time when the main loop is started
+#Time when the main loop was last executed
 now_time = datetime.now()
 
-while now_time < start_time + timedelta(minutes=2):
+while (now_time < start_time + timedelta(minutes=2)):
     image_file = f"{base_folder}/photos/image_{counter:04d}.jpg"
-    main_data.write(f"Took image_{counter:04d}.jpg in {datetime.now()}")
+    camera.capture(image_file)
+    main_data.write(f"Took image_{counter:04d}.jpg in {datetime.now()} \n")
+    print(f"Took image_{counter:04d}.jpg in {datetime.now()} \n")
     counter += 1
     sleep(10)
+    now_time = datetime.now()
