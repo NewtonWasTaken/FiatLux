5d345abfeeb36ccd5caa757c2b092b870972b254
NewtonWasTaken <j.graffe@seznam.cz>
2023-02-01 21:02:22 +0100

    Added the function to take pictures with EXIF data about the location. Added the feature about have often we will take the pictures, depending on day or night.

--- main.py
+++ main.py
@@ -34,27 +34,69 @@ counter = 0
 now_time = datetime.now()
 
 
+def take_photo(counter, location):
+    """
+    Takes photo, which has asigned the EXIF data
+    :param counter: Takes counter from main loop
+    :param location: Takes ISS.coordinates object
+    :return:
+    """
+
+    #Location of the photo
+    image_file = f"{base_folder}/photos/image_{counter:04d}.jpg"
+
+    #Convert the location data to EXIF format
+    south, exif_latitude = convert(location.latitude)
+    west, exif_longitude = convert(location.longitude)
+
+    #Adds the EXIF data to the photo
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    #Capture the photo
+    camera.capture(image_file)
+
+def convert(angle):
+    """
+    Converts angle from ISS.location object to an EXIF format
+    :param angle: Takes one angle from ISS.coordinates object
+    :return: Returns boolean and converted angle. Boolean says if the angle is negative (True) or positive (False)
+    """
+
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+
+    return sign < 0, exif_angle
+
+
 #Main loop of the program
 while (now_time < start_time + timedelta(minutes=180)):
-    try:  
+    try:
+        #Loads the ISS location in current time
         location = ISS.coordinates() 
-        
+
+        #Takes photo and sings the information about location into to photo
         take_photo(counter, location)
 
+        #Completes cycle
         counter += 1
+
+        # Decides wheater to take pictures in night or in day
         timescale = load.timescale()
         t = timescale.now()
         if ISS.at(t).is_sunlit(ephemeris):
             sleep(15)
         else:
             sleep(20)
+
+        #Sets the current time
         now_time = datetime.now()
 
     except Exception as e:
         logger.error(f'{e.__class__.__name__}: {e})')
 
-#Takes photo and logs it into files
-def take_photo(counter, location):
-    image_file = f"{base_folder}/photos/image_{counter:04d}.jpg"
-    camera.capture(image_file)
+
+
     
